d4862192c270237ca762cf814266afdd1b903d45
Charles Martin <cpascual185@gmail.com>
2025-08-26 22:21:12 +0800

    Rename a docker for a proper naming

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -1,6 +1,8 @@
 docker compose down -v --rmi all --remove-orphans
 docker builder prune -af
-docker compose up --build
+# docker compose up --build
+docker compose build
+docker compose up -d
 
 docker exec -it hitchhikers_app php artisan storage:link
 docker exec -it hitchhikers_app php artisan migrate
