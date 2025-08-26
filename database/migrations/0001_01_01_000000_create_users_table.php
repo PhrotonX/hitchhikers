612dfdea5ab0b7318de3b55b4e06b67920feb301
Charles Martin <cpascual185@gmail.com>
2025-08-26 16:37:01 +0800

    Add Picture model and all related classes.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
+            $table->softDelete();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
