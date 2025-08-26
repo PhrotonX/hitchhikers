fdb83eae211bb633b992492edeb06cf5b89751c0
Charles Martin <cpascual185@gmail.com>
2025-08-26 16:24:17 +0800

    Add Vehicle modela nd all related classes.

---
+++ database/migrations/2025_08_26_080550_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('plate_number')->unique();
+            $table->string('vehicle_model');
+            $table->string('vehicle_brand');
+            $table->integer('capacity');
+            $table->string('coordinates')->nullable();
+            $table->string('color');
+            $table->string('type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
