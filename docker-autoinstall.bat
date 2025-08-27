2d7ba6bac309420a20773fa32cb5459f9e77cf89
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-08-27 20:20:56 +0800

    Update docker-autoinstall.bat
    Add more timeouts to avoid race conditions.

--- docker-autoinstall.bat
+++ docker-autoinstall.bat
@@ -12,12 +12,18 @@ REM docker compose up --build
 docker compose build
 docker compose up -d
 
+echo Please wait...
+timeout /t 15 >nul
+
 REM Batch 2
 docker exec -it hitchhikers_app composer install
 docker exec -it hitchhikers_node npm install
 
 docker exec -it hitchhikers_app php artisan migrate
 docker exec -it hitchhikers_app php artisan storage:link
+
+echo Please wait...
+timeout /t 20 >nul
 
 REM Batch 3 (Key generation)
 docker exec -it hitchhikers_app php artisan key:generate
