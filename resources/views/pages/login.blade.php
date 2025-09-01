c27bff7e29798c469e6c993b7ead2d8622bb9754
Charles Martin <cpascual185@gmail.com>
2025-09-01 23:56:33 +0800

    Add login page

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,3 +1,12 @@
+@extends('layouts.app')
 @section('content')
-    <input type="text" name="login">
+    <form action="api/login" method="POST">
+        @csrf
+        <label>Email</label>
+        <input type="text" name="email">
+        <label>Password</label>
+        <input type="password" name="password">
+        <button type="submit">Submit</button>
+    </form>
+    <button type="button">Create an account</button>
 @endsection
