9f253115a3f0858a6e34c42e7f83bec19792b3c0
Charles Martin <cpascual185@gmail.com>
2025-09-02 13:28:44 +0800

    Add language files

---
+++ lang/en/gender.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Gender Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the gender words used in user account data.
+    |
+    */
+
+    'male' => 'Male',
+    'female' => 'Female',
+    'prefer_not_to_say' => 'Prefer not to say',
+];
