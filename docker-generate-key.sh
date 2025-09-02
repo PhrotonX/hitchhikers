535abc77c63fd9da6bc3a9ca3a3a87e69e600bde
Charles Martin <cpascual185@gmail.com>
2025-09-02 15:44:22 +0800

    Fix docker configuration issues

--- docker-generate-key.sh
+++ docker-generate-key.sh
@@ -5,3 +5,6 @@ docker exec -it hitchhikers_app php artisan cache:clear
 docker exec -it hitchhikers_app php artisan config:cache
 docker compose down -v
 docker compose up -d
+
+#Wait for 15-20 seconds...
+docker exec -it hitchhikers_app php artisan migrate
