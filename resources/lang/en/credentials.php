90a4ca425c6edf22b8fc75dfc9c807bf33a54110
Charles Martin <cpascual185@gmail.com>
2025-09-06 15:45:12 +0800

    Fix language files not being read

---
+++ resources/lang/en/credentials.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Credentials Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the first section of 
+    | the event booking page and login or sign up page.
+    |
+    */
+
+    'account' => 'Account',
+    'delete_account' => 'Delete Account',
+    'delete_account_msg' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+    'update_profile_information' => 'Update Profile Information',
+    'update_profile_information_msg' => 'Update your account\'s profile information and email address.',
+    'email_password_reset_link' => 'Email Password Reset Link',
+    'create_an_account' => 'Create an Account',
+    'existing_account_log_in' => 'Existing Account? Log In',
+    'register' => 'Register',
+    'profile_picture' => 'Profile Picture',
+    'choose_from_uploads' => 'Choose from Uploads',
+    'edit_profile' => 'Edit Profile',
+    'reset_password' => 'Reset Password',
+    'delete_account_warning' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.',
+    'delete_account_info' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+    'delete_account_confirmation' => 'Are you sure you want to delete your account?',
+    'email_unverified' => 'Your email address is unverified.',
+    'email_resend_verification' => 'Click here to re-send the verification email.',
+    'verification_link_sent' => 'A new verification link has been sent to your email address.',
+    'resend_verification_email' => 'Click here to re-send the verification email.',
+];
