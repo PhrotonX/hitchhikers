d93987a8314f1fb86922790c4baf21f91b702b25
Charles Martin <cpascual185@gmail.com>
2025-09-06 23:29:37 +0800

    Add forgot password page and related code

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -18,5 +18,12 @@ return [
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
-
+    'password_reset_form' => 'Password Reset Form',
+    'password_reset_form_text' => 'Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.',
+    'password_confirmation' => 'Confirm your Password',
+    'password_confirmation_hint' => 'Enter your password...',
+    'secure_area_of_app' => 'This is a secure area of the application. Please confirm your password before continuing.',
+    'confirm_password' => 'Confirm Password',
+    'update_password' => 'Update Password',
+    'update_password_msg' => 'Ensure your account is using a long, random password to stay secure.',
 ];
