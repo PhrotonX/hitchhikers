aa0110d587eb6aab148a883c0c1f1c5fe50654c7
Charles Martin <cpascual185@gmail.com>
2025-09-09 21:10:33 +0800

    Add additional language file

---
+++ resources/lang/en/string.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common String Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are common strings found within the entire website.
+    |
+    */
+
+    'submit' => 'Submit',
+    'ok' => 'OK',
+    'cancel' => 'Cancel',
+    'back' => 'Back',
+    'enroll_to_driving_program' => 'Enroll to Driving Program',
+];
