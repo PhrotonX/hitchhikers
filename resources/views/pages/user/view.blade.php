5ef5613b8e465d45e8b08b063aec36411f21deca
Charles Martin <cpascual185@gmail.com>
2025-09-09 22:55:56 +0800

    Add settings page

--- resources/views/pages/user/view.blade.php
+++ resources/views/pages/user/view.blade.php
@@ -4,19 +4,7 @@
     <p><strong>Date joined: </strong>{{Auth::user()->created_at}}</p>
 
     @auth
-        <a href="/user/{{Auth::user()->id}}/edit">Edit</a>
-        @if (auth()->user() instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! auth()->user()->hasVerifiedEmail())
-            <div>
-                <p>
-                    {{ __('credentials.email_unverified_msg') }}
-                </p>
-                <button type="button" id="verify-email-btn" onclick="window.location.href='{{route('verification.notice')}}'">
-                    <p>{{ __('credentials.verify_email') }}</p>
-                </button>
-            </div>
-        @endif
-
-        <a href="/user/{{Auth::user()->id}}/delete">Delete</a>
+        <a href="/settings">Settings</a>
     @endauth
     
 @endsection
