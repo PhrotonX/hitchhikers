65048d79a2832cf504637531e7afc9f0ca7ab266
Charles Martin <cpascual185@gmail.com>
2025-09-14 23:30:02 +0800

    Update Vehicle model class

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -9,4 +9,14 @@ class Vehicle extends Model
 {
     /** @use HasFactory<\Database\Factories\VehicleFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'plate_number',
+        'vehicle_model',
+        'vehicle_brand',
+        'capacity',
+        'coordinates',
+        'color',
+        'type',
+    ];
 }
