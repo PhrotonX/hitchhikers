2ef29b03a8217f3ea55e387861c5a928eca3a56a
Charles Martin <cpascual185@gmail.com>
2025-09-15 09:46:00 +0800

    Implement VehicleController.stor() and create() functions.

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreVehicleRequest;
 use App\Http\Requests\UpdateVehicleRequest;
 use App\Models\Vehicle;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\JsonResponse;
 
 class VehicleController extends Controller
 {
@@ -21,15 +23,42 @@ class VehicleController extends Controller
      */
     public function create()
     {
-        //
+        return redirect()->route('pages.vehicle.create');
+    }
+
+    /**
+     * Store a newly created resource in storage
+     * 
+     * @return RedirectResponse Page for creating vehicles.
+     */
+    public function store(StoreVehicleRequest $request): RedirectResponse
+    {
+        $this->onStore($request);
+
+        return redirect()->route('pages.vehicle.create');
+    }
+
+    /**
+     * Store a newly created resource in storage and returns a 
+     * @return JsonResponse Consists of ```redirect``` for creating vehicles.
+     */
+    public function storeAPI(StoreVehicleRequest $request): JsonResponse{
+        $this->onStore($request);
+
+        return response()->json([
+            'redirect' => 'pages.vehicle.create',
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreVehicleRequest $request)
-    {
-        //
+    protected function onStore(StoreVehicleRequest $request){
+        $vehicle = new Vehicle();
+
+        $vehicle->fill($request->validated());
+
+        $vehicle->save();
     }
 
     /**
