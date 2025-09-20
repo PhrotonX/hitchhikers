f9697f555d73d26ff18ef6e8b285d52a21e94c12
Charles Martin <cpascual185@gmail.com>
2025-09-20 21:58:54 +0800

    Improve form page for creating vehicles

---
+++ resources/lang/en/vehicle_type.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Types of vehicles
+ */
+
+return [
+    'bus' => 'Bus',
+    'car' => 'Car',
+    'e-bike' => 'e-Bike',
+    'minibus' => 'Minibus',
+    'minivan' => 'Minivan',
+    'motorcycle' => 'Motorcycle',
+    'suv' => 'SUV (Sport Utility Vehicle)',
+    'tricycle' => 'Tricycle',
+    'tuktuk' => 'Tuktuk',
+    'van' => 'Van',
+];
