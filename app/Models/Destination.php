e2d8d1415c4e5a2ab1881ba3f7e609e33770888c
Charles Martin <cpascual185@gmail.com>
2025-09-21 18:30:33 +0800

    Add Destination model and related classes.

---
+++ app/Models/Destination.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    /** @use HasFactory<\Database\Factories\DestinationFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'longitude',
+        'latitude',
+        'order',
+    ];
+}
