a866c4012ce70b73aaa648824faff4486b731815
Charles Martin <cpascual185@gmail.com>
2025-09-24 13:01:52 +0800

    Use Leaflet Control Geocoder

--- resources/views/components/map-head.blade.php
+++ resources/views/components/map-head.blade.php
@@ -1,10 +1,17 @@
 @push('head')
+    {{-- Leaflet.js --}}
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
         crossorigin=""></script>
+
+    {{-- Leaftlet Control Geocoder --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+    <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+    
+    {{-- Leaflet Default Map Style --}}
     <style>
         #map {
             width: 100%;
--- resources/views/pages/ride/create.blade.php
+++ resources/views/pages/ride/create.blade.php
@@ -4,86 +4,110 @@
 
 @section('content')
     <h1>{{__('string.create_ride')}}</h1>
-    
-    <x-input-label>{{__('string.ride_name')}}</x-input-label>
-    <x-text-input
-        :name="'ride_name'"
-        :placeholder="__('string.ride_name_placeholder')"
-        :value="old('ride_name')"
-        :required="true"
-    /><br>
-    <x-input-error :messages="$errors->get('ride_name')"/>
 
-    <x-input-label>{{__('string.fare_rate')}}</x-input-label>
-    <x-text-input
-        :name="'fare_rate'"
-        :placeholder="__('string.fare_rate_placeholder')"
-        :value="old('fare_rate')"
-        :required="true"
-    /><br>
-    <x-input-error :messages="$errors->get('fare_rate')"/>
+    <form action="/ride/create/submit" method="POST">
+        @csrf
 
-    <x-input-label>{{__('string.vehicle')}}</x-input-label>
-    <select name="vehicle_id" title="{{__('string.vehicle')}}">
-        @foreach ($driverVehicles as $key => $value)
-            <option value="{{$value->vehicle->id}}">{{$value->vehicle->vehicle_name}}</option>
-        @endforeach
-    </select>
-    <x-input-error :messages="$errors->get('vehicle_id')"/>
+        <x-input-label>{{__('string.ride_name')}}</x-input-label>
+        <x-text-input
+            :name="'ride_name'"
+            :placeholder="__('string.ride_name_placeholder')"
+            :value="old('ride_name')"
+            :required="true"
+        /><br>
+        <x-input-error :messages="$errors->get('ride_name')"/>
 
-    <h2>{{__('string.destinations')}}</h2>
+        <x-input-label>{{__('string.fare_rate')}}</x-input-label>
+        <x-text-input
+            :name="'fare_rate'"
+            :placeholder="__('string.fare_rate_placeholder')"
+            :value="old('fare_rate')"
+            :required="true"
+        /><br>
+        <x-input-error :messages="$errors->get('fare_rate')"/>
 
-    <div class="destination-selector">
-        <div id="map"></div>
+        <x-input-label>{{__('string.vehicle')}}</x-input-label>
+        <select name="vehicle_id" title="{{__('string.vehicle')}}">
+            @foreach ($driverVehicles as $key => $value)
+                <option value="{{$value->vehicle->id}}">{{$value->vehicle->vehicle_name}}</option>
+            @endforeach
+        </select>
+        <x-input-error :messages="$errors->get('vehicle_id')"/>
 
-        <script>
-            var map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16});
+        <h2>{{__('string.destinations')}}</h2>
 
-            //Define the marker icon.
-            var markerIcon = L.icon({
-                iconUrl: '{{Vite::asset("resources/img/red_pin.png")}}',
-                shadowUrl: '{{Vite::asset("resources/img/shadow_pin.png")}}',
+        <div class="destination-selector">
+            <div id="map"></div>
+
+            {{-- TODO: Make this code reusable by encapsulating it, making it OOP and move it into Map.js --}}
+            <script>
+                var map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16});
+
+                //Define the marker icon.
+                var markerIcon = L.icon({
+                    iconUrl: '{{Vite::asset("resources/img/red_pin.png")}}',
+                    shadowUrl: '{{Vite::asset("resources/img/shadow_pin.png")}}',
+                    
+                    iconSize:     [38, 95],
+                    shadowSize:   [50, 64],
+                    iconAnchor:   [22, 94],
+                    shadowAnchor: [4, 62], 
+                    popupAnchor:  [-3, -76]
+                });
+
+                // Insert Leaftlet Control Geocoder Plugin.
+                // new L.Control.Geocoder().addTo(map);
+
+                // Handle map markers
+                map.on('click', function(e){
+                    console.log('Coordinates: ' + e.latlng.lat + ", " + e.latlng.lng);
+                    // var results = L.Control.geocoders.geocoder.reverse(e.latlng, map.getZoom());
+                    // console.log('Results: ' + results);
+
+                    //Add markers
+                    L.marker([e.latlng.lat, e.latlng.lng], {icon: markerIcon}).addTo(map);
+
+                    //Add to destination list.
+                    var destinationList = document.getElemenById('destination-list');
+
+                        var destinationItem = document.createElement('div');
+                        
+                            var destinationName = document.createElement('p');
+                            // @TODO: Obtain destination name here using Leaflet.
+                            // destinationName.innerHTML = 
+
+                            var destinationCoordinates = document.createElement('p');
+                            destinationCoordinates.innerHTML = 'Coordinates: ' + e.latlng.lat + ", " + e.latlng.lng;
+                            destinationItem.appendChild(destinationItem);
+                });
+
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+
+                // Configure geocoder
+                // var geocoder = L.Control.geocoder({
+                //     defaultMarkGeocode: false
+                // }).on('markgeocode', function(e){
+                    
+                // })
                 
-                iconSize:     [38, 95],
-                shadowSize:   [50, 64],
-                iconAnchor:   [22, 94],
-                shadowAnchor: [4, 62], 
-                popupAnchor:  [-3, -76]
-            });
+            </script>
+        </div>
 
-            map.on('click', function(e){
-                console.log('Coordinates: ' + e.latlng.lat + ", " + e.latlng.lng);
-
-                //Add markers
-                L.marker([e.latlng.lat, e.latlng.lng], {icon: markerIcon}).addTo(map);
-            });
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-        </script>
-    </div>
+        <div class="destination-list">
+            <h3>{{__('string.from')}}</h3>
+            <h3>{{__('string.to')}}</h3>
+            
+        </div>
+    </form>
     
-
-    {{-- Must be able to add or remove an address. --}}
-    {{-- Must be draggable. Each drag should change the destination value based on their positions. --}}
-    {{-- <x-input-label>{{__('string.unit_no')}}</x-input-label>
-    <x-text-input
-        :name="'unit_no'"
-        :placeholder="__('string.unit_no_placeholder')"
-        :value="old('unit_no')"
-        :required="true"
-    />
-    <x-input-error :messages="$errors->get('unit_no')"/>
-
-    <x-input-label>{{__('string.unit_no')}}</x-input-label>
-    <x-text-input
-        :name="'unit_no'"
-        :placeholder="__('string.unit_no_placeholder')"
-        :value="old('unit_no')"
-        :required="true"
-    />
-    <x-input-error :messages="$errors->get('unit_no')"/> --}}
-
+    
 @endsection
+
+@push('scripts')
+    <script>
+        
+    </script>
+@endpush
