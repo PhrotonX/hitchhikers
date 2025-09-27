9a6eac0406e3724e6f9ab4bd8a3d4c7ab3ec7d6a
Charles Martin <cpascual185@gmail.com>
2025-09-27 22:40:00 +0800

    Rename DestinationController into RideDestinationController and partially implement its index() function.

---
+++ app/Http/Controllers/RideDestinationController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Http\Requests\StoreDestinationRequest;
+// use App\Http\Requests\UpdateDestinationRequest;
+use App\Models\RideDestination;
+use Illuminate\Http\Request;
+
+class RideDestinationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // Retrieve URL parameters.
+        $north = $request->input('lat-north');
+        $south = $request->input('lat-south');
+        $east = $request->input('lng-east');
+        $west = $request->input('lng-west');
+
+        // Get rides based on a range of coordinates that forms a bounding box.
+        $results = RideDestination::query('latitude', 'BETWEEN', $north, 'AND', $south, 'AND',
+            'longitude', 'BETWEEN', $east, 'AND', $west);
+
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    // public function store(StoreDestinationRequest $request)
+    // {
+    //     //
+    // }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Destination $destination)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    // public function edit(Destination $destination)
+    // {
+    //     //
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    // public function update(UpdateDestinationRequest $request, Destination $destination)
+    // {
+    //     //
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    // public function destroy(Destination $destination)
+    // {
+    //     //
+    // }
+}
