9a272ef97274b0ee5cacb51e9ca94ec1004b4ca6
Charles Martin <cpascual185@gmail.com>
2025-10-31 22:42:11 +0800

    Update Reply model

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,13 +7,14 @@ use App\Models\Model;
 class Reply extends Model{
     protected static $table = "replies";
     protected static $object = Reply::class;
-    // protected static $fillable = [
-    //     'description',
-    // ];
-
-    public ?int $mentioned_account_id;
-    public string $description;
-    public ?object $created_at;
-    public ?object $updated_at;
-    public ?object $deleted_at;
+    protected static $fillable = [
+        'replied_review_id',
+        'mentioned_account_id',
+        'description',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'user_id',
+        'ride_id',
+    ];
 }
