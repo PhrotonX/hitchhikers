8197fbfaa9565815cf1889a26f0c89e959fd662c
Charles Martin <cpascual185@gmail.com>
2025-11-08 18:56:38 +0800

    Add models for saved_rides and saved_ride_folders table.

---
+++ app/Models/SavedRideFolder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Model;
+
+class SavedRideFolder extends Model{
+    protected static $table = "saved_ride_folders";
+    protected static $object = SavedRideFolder::class;
+    protected static $fillable = [
+        'name',
+        'color',
+        'icon',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
+}
