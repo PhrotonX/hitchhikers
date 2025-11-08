6547bb5b0cc6647de5e44461a43beea423dc70c1
Charles Martin <cpascual185@gmail.com>
2025-11-08 21:20:01 +0800

    Add incomplete implementation of AssociativeModel

--- app/Models/SavedRideFolderItems.php
+++ app/Models/SavedRideFolderItems.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use App\Models\Model;
 
-class SavedRideFolderItems extends Model{
+class SavedRideFolderItems extends Associative{
     protected static $table = "saved_ride_folder_items";
     protected static $object = SavedRideFolderItems::class;
+    protected static $primary = ['saved_ride_id', 'saved_ride_folder_id'];
 }
