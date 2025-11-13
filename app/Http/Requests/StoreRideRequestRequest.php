655385ccd65d3857d57b919d2fa5613800662e0d
Charles Martin <cpascual185@gmail.com>
2025-11-13 23:41:11 +0800

    Add StoreRideRequestRequest

---
+++ app/Http/Requests/StoreRideRequestRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRideRequestRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'ride_id' => ['required', 'numeric'],
+            'sender_user_id' => ['required', 'numeric'],
+            'destination_id' => ['nullable', 'numeric'],
+            'from_latitude' => ['nullable', 'decimal:10,7'],
+            'from_longitude' => ['nullable', 'decimal:10,7'],
+            'to_latitude' => ['nullable', 'decimal:10,7'],
+            'to_longitude' => ['nullable', 'decimal:10,7'],
+            'pickup_at' => ['required', 'string', 'max:255'],
+            'time' => ['required'],
+            'message' => ['nullable', 'string', 'max:5000'],
+        ];
+    }
+}
