af7f2914547f3676c7df02e372cf71afaa19901a
Charles Martin <cpascual185@gmail.com>
2025-11-18 16:17:40 +0800

    Revert changes and add ride-request div

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -86,7 +86,23 @@
     </div>
 
     <div id="ride-request" hidden>
+        <button id="ride-request-close">Close</button>
         <h2>Make a Ride Request</h2>
+        <p><strong>Destination: </strong><span id="ride-request-destination"></span></p>
+        <p><strong>Description: </strong><span id="ride-request-description"></span></p>
+        <!-- <p><strong>Currently on: </strong><span id="ride-request-location"></span></p> -->
+        
+        <p>Click on the map to choose your ride destination.</p>
+        <label for="pickup_at">Pickup At:</label>
+        <input type="text" name="pickup_at" id="ride-request-pickup-at"></input>
+
+        <label for="time">Pickup Time:</label>
+        <input type="time" name="time" id="ride-request-time"></input>
+
+        <label for="message">Message (Optional):</label>
+        <textarea name="message" id="ride-request-message"></textarea>
+
+        <button type="button">Submit</button>
     </div>
 @endsection
 
